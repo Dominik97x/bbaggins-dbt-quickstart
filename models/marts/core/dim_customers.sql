a2702a45f41a5770889facfc9a2da0a680003ec3
Dominik Halista <d.halista@onet.pl>
2024-02-19 09:21:54 +0000

    Created stg_payments and fct_orders. Also created another version of dim_customers.

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -14,10 +14,10 @@ orders as(
 
         select
             customer_id,
-
             min(order_date) as first_order_date,
             max(order_date) as most_recent_order_date,
             count(order_id) as number_of_orders
+    
 
         from orders
 
@@ -34,10 +34,11 @@ orders as(
             customer_orders.first_order_date,
             customer_orders.most_recent_order_date,
             coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-
+        
         from customers
 
         left join customer_orders using (customer_id)
+        
 
     )
 
